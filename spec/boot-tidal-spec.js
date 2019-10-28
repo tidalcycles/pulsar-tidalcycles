dd3c07a35376bbbdcd4477e91a997ce4fde17c98
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2019-10-28 13:20:58 +0100

    Add fallback to atom package's BootTidal.hs path when the one resolved from resolver does not exists

---
+++ spec/boot-tidal-spec.js
@@ -0,0 +1,70 @@
+const BootTidal = require('../lib/boot-tidal.js')
+const fs = require('fs')
+const child_process = require('child_process')
+
+describe('boot-tidal', () => {
+  let bootTidal = new BootTidal([{ path: '/current/directory' }])
+
+  beforeEach(() => {
+    this.originalPlatform = process.platform;
+    waitsForPromise(() => atom.packages.activate('tidalcycles'))
+  })
+
+  afterEach(() => {
+    setPlatform(this.originalPlatform)
+  })
+
+  describe('boot file sequence', () => {
+
+    it(`should choose current directory BootTidal.hs if it exists`, () => {
+      spyOn(fs, 'existsSync').andReturn(true)
+
+      expect(bootTidal.choosePath()).toBe('/current/directory/BootTidal.hs')
+    })
+
+    it(`should choose custom boot file when provided`, () => {
+      atom.config.set('tidalcycles.bootTidalPath', '/custom/directory/BootTidal.hs')
+      spyOn(fs, 'existsSync').andReturn(true)
+
+      expect(bootTidal.choosePath()).toBe('/custom/directory/BootTidal.hs')
+    })
+
+    it('should choose tidal installation boot file when OS is a known one', () => {
+      setPlatform('linux')
+      spyOn(fs, 'existsSync').andCallFake(path => {
+        switch (path) {
+          case '/posix/tidal/BootTidal.hs': return true;
+          default: return false;
+        }
+      });
+      spyOn(child_process, 'execSync').andReturn('/posix/tidal\n')
+
+      expect(bootTidal.choosePath()).toBe('/posix/tidal/BootTidal.hs')
+    })
+
+    it('should choose default boot file when no custom provided, no file in current directory and platform resolved file does not exist', () => {
+      setPlatform('linux')
+      spyOn(fs, 'existsSync').andCallFake(path => {
+        switch (path) {
+          case '/posix/tidal/BootTidal.hs': return false;
+          default: return false;
+        }
+      });
+      spyOn(child_process, 'execSync').andReturn('/posix/tidal\n')
+
+      expect(bootTidal.choosePath()).toContain('/lib/BootTidal.hs')
+    })
+
+    it(`should choose default boot file when no custom provided, no file in current directory and platform is unknown`, () => {
+      setPlatform('unknown')
+      spyOn(fs, 'existsSync').andReturn(false)
+
+      expect(bootTidal.choosePath()).toContain('/lib/BootTidal.hs')
+    })
+
+  })
+})
+
+function setPlatform(name) {
+  Object.defineProperty(process, 'platform', { value: name });
+}
