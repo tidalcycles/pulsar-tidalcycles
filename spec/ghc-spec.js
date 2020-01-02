b94255e7f049a0a8a92353d8920f5fc6dc589b40
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-01-02 08:47:23 +0100

    Add logs to ghc

--- spec/ghc-spec.js
+++ spec/ghc-spec.js
@@ -4,6 +4,8 @@ const Ghc = require('../lib/ghc')
 
 describe('ghc', () => {
 
+  let ghc = new Ghc({ logStdout: () => {} })
+
   beforeEach(() => {
     waitsForPromise(() => atom.packages.activate('tidalcycles'))
   })
@@ -12,7 +14,7 @@ describe('ghc', () => {
     it(`should choose ghcup path by default`, () => {
       spyOn(fs, 'existsSync').andReturn(true)
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.interactivePath).toBe('~/.ghcup/bin/ghci')
       expect(ghc.pkgPath).toBe('~/.ghcup/bin/ghc-pkg')
@@ -21,7 +23,7 @@ describe('ghc', () => {
     it(`should be itself if ghcup path does not exists`, () => {
       spyOn(fs, 'existsSync').andReturn(false)
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.interactivePath).toBe('ghci')
       expect(ghc.pkgPath).toBe('ghc-pkg')
@@ -30,7 +32,7 @@ describe('ghc', () => {
     it(`should be consistent with ghciPath property if it exists`, () => {
       atom.config.set('tidalcycles.ghciPath', '/some/path/ghci')
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.interactivePath).toBe('/some/path/ghci')
       expect(ghc.pkgPath).toBe('/some/path/ghc-pkg')
@@ -39,7 +41,7 @@ describe('ghc', () => {
     it(`should append command to ghciPath property if this indicates a folder`, () => {
       atom.config.set('tidalcycles.ghciPath', '/some/path')
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.interactivePath).toBe('/some/path/ghci')
       expect(ghc.pkgPath).toBe('/some/path/ghc-pkg')
@@ -48,7 +50,7 @@ describe('ghc', () => {
     it(`should handle ghciPath property terminating with path separator`, () => {
       atom.config.set('tidalcycles.ghciPath', '/some/path/')
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.interactivePath).toBe('/some/path/ghci')
       expect(ghc.pkgPath).toBe('/some/path/ghc-pkg')
@@ -57,7 +59,7 @@ describe('ghc', () => {
     it(`should handle stack exec path style`, () => {
       atom.config.set('tidalcycles.ghciPath', 'stack exec --package tidal – ghci')
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.interactivePath).toBe('stack exec --package tidal – ghci')
       expect(ghc.pkgPath).toBe('stack exec --package tidal – ghc-pkg')
@@ -68,7 +70,7 @@ describe('ghc', () => {
     it('should take data-dir path from ghc-pkg output', () => {
       spyOn(child_process, 'execSync').andReturn('data-dir: /some/path/tidal\n')
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.tidalDataDir()).toBe('/some/path/tidal')
     })
@@ -76,7 +78,7 @@ describe('ghc', () => {
     it('should handle spaces in path', () => {
       spyOn(child_process, 'execSync').andReturn('data-dir: /some/pa th/tidal\n')
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.tidalDataDir()).toBe('/some/pa th/tidal')
     })
@@ -84,7 +86,7 @@ describe('ghc', () => {
     it(`should return what it gets when there's no path in ghc-pkg path`, () => {
       spyOn(child_process, 'execSync').andReturn('no-path-in-this-output\n')
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.tidalDataDir()).toBe('no-path-in-this-output')
     })
@@ -94,7 +96,7 @@ describe('ghc', () => {
         throw Error("error")
       })
 
-      let ghc = new Ghc()
+      ghc.init()
 
       expect(ghc.tidalDataDir()).toBe('')
     })
