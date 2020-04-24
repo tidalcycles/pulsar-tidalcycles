6bd655901f3f1bd0c2248d67b0b228496dea18ef
Andrea Bertagnolli <andrea.bertagnolli@cherrychain.it>
2020-04-24 15:22:07 +0200

    Add interpreter configuration with which it's possible to select an alternative interpreter (nix or stack)

--- spec/ghc-spec.js
+++ spec/ghc-spec.js
@@ -66,35 +66,36 @@ describe('ghc', () => {
       expect(ghc.pkgPath).toBe('/some/path/ghc-pkg')
     })
 
-    it(`should handle stack exec path style`, () => {
-      atom.config.set('tidalcycles.ghciPath', 'stack exec --package tidal – ghci')
+    it(`should handle stack interpreter`, () => {
+      atom.config.set('tidalcycles.interpreter', 'stack')
 
       ghc.init()
 
-      expect(ghc.interactivePath).toBe('stack exec --package tidal – ghci')
-      expect(ghc.pkgPath).toBe('stack exec --package tidal – ghc-pkg')
+      expect(ghc.interactivePath).toBe('stack exec --package tidal ghci')
+      expect(ghc.pkgPath).toBe('stack exec --package tidal ghc-pkg')
     })
 
-    it(`should handle nix-shell path style`, () => {
-      atom.config.set('tidalcycles.ghciPath', 'nix-shell -p "haskellPackages.ghcWithPackages (pkgs: [pkgs.tidal])" --run ghci')
+    it(`should handle nix interpreter`, () => {
+      atom.config.set('tidalcycles.interpreter', 'nix')
 
       ghc.init()
 
       expect(ghc.interactivePath).toBe('nix-shell -p "haskellPackages.ghcWithPackages (pkgs: [pkgs.tidal])" --run ghci')
-      expect(ghc.pkgPath).toBe('nix-shell -p "haskellPackages.ghcWithPackages (pkgs: [pkgs.tidal])" --run ghc-pkg')
+      expect(ghc.pkgPath).toBe('nix-shell -p "haskellPackages.ghcWithPackages (pkgs: [pkgs.tidal])" --run "ghc-pkg"')
     })
   })
 
   describe('tidal data dir', () => {
-    it('should take data-dir path from ghc-pkg output', () => {
+    it('should take data-dir path from ghc-pkg output wrapped in double quotes for default interpreter', () => {
       spyOn(child_process, 'execSync').andReturn('data-dir: /some/path/tidal\n')
 
       ghc.init()
+      let tidalDataDir = ghc.tidalDataDir()
 
-      expect(ghc.tidalDataDir()).toBe('/some/path/tidal')
+      expect(tidalDataDir).toBe('/some/path/tidal')
     })
 
-    it('should handle spaces in path', () => {
+    it('should handle spaces in returning path', () => {
       spyOn(child_process, 'execSync').andReturn('data-dir: /some/pa th/tidal\n')
 
       ghc.init()
@@ -118,6 +119,70 @@ describe('ghc', () => {
       ghc.init()
 
       expect(ghc.tidalDataDir()).toBe('')
+    })
+
+    it(`should wrap ghc-pkg in double quotes for default interpreter to handle whitespaces in path`, () => {
+      spyOn(child_process, 'execSync')
+      atom.config.set('tidalcycles.interpreter', 'default')
+      atom.config.set('tidalcycles.ghciPath', '/path with whitespace')
+
+      ghc.init()
+      ghc.tidalDataDir()
+
+      expect(child_process.execSync.calls[0].args[0]).toBe('"/path with whitespace/ghc-pkg" field tidal data-dir')
+    })
+
+    it(`should not wrap ghc-pkg in double quotes for stack interpreter`, () => {
+      spyOn(child_process, 'execSync')
+      atom.config.set('tidalcycles.interpreter', 'stack')
+
+      ghc.init()
+      ghc.tidalDataDir()
+
+      expect(child_process.execSync.calls[0].args[0]).toBe('stack exec --package tidal ghc-pkg field tidal data-dir')
+    })
+
+    it(`should not wrap ghc-pkg in double quotes for nix interpreter`, () => {
+      spyOn(child_process, 'execSync')
+      atom.config.set('tidalcycles.interpreter', 'nix')
+
+      ghc.init()
+      ghc.tidalDataDir()
+
+      expect(child_process.execSync.calls[0].args[0]).toBe('nix-shell -p "haskellPackages.ghcWithPackages (pkgs: [pkgs.tidal])" --run "ghc-pkg field tidal data-dir"')
+    })
+  })
+
+  describe('browseTidal', () => {
+    it(`should wrap ghci path with double quotes for default interpreter to handle whitespace-in-path`, () => {
+      atom.config.set('tidalcycles.interpreter', 'default')
+      atom.config.set('tidalcycles.ghciPath', '/path whitespace/')
+      spyOn(child_process, 'exec')
+
+      ghc.init()
+      ghc.browseTidal(() => {})
+
+      expect(child_process.exec.calls[0].args[0]).toBe('echo ":browse Sound.Tidal.Context" | "/path whitespace/ghci"')
+    })
+
+    it(`should not wrap ghci path with double quotes for stack interpreter`, () => {
+      atom.config.set('tidalcycles.interpreter', 'stack')
+      spyOn(child_process, 'exec')
+
+      ghc.init()
+      ghc.browseTidal(() => {})
+
+      expect(child_process.exec.calls[0].args[0]).toBe('echo ":browse Sound.Tidal.Context" | stack exec --package tidal ghci')
+    })
+
+    it(`should not wrap ghci path with double quotes for nix interpreter`, () => {
+      atom.config.set('tidalcycles.interpreter', 'nix')
+      spyOn(child_process, 'exec')
+
+      ghc.init()
+      ghc.browseTidal(() => {})
+
+      expect(child_process.exec.calls[0].args[0]).toBe('echo ":browse Sound.Tidal.Context" | nix-shell -p "haskellPackages.ghcWithPackages (pkgs: [pkgs.tidal])" --run ghci')
     })
   })
 
