655205a8535a1befd9a35ae245e6e24b1e4579af
ndr_brt <andrea.bertagnolli@cherrychain.it>
2021-01-03 18:06:10 +0100

    Implement mute single connections

--- spec/repl-spec.js
+++ spec/repl-spec.js
@@ -9,7 +9,6 @@ describe('repl', () => {
   })
 
   describe('init tidal', () => {
-
     it('should load BootTidal with :script', () => {
       spyOn(repl, 'tidalSendLine')
 
@@ -17,6 +16,26 @@ describe('repl', () => {
 
       expect(repl.tidalSendLine.callCount).toBe(1)
       expect(repl.tidalSendLine.calls[0].args[0]).toBe(':script lib/BootTidal.hs')
+    })
+  })
+
+  describe('toggle mute', () => {
+    it('should mute a connection not already muted', () => {
+      spyOn(repl, 'tidalSendLine')
+
+      repl.toggleMute('3')
+
+      expect(repl.eval.callCount).toBe(1)
+      expect(repl.eval.calls[0].args[1]).toBe('mute 3')
+    })
+
+    it('should map connection 0 as 10', () => {
+      spyOn(repl, 'tidalSendLine')
+
+      repl.toggleMute('0')
+
+      expect(repl.eval.callCount).toBe(1)
+      expect(repl.eval.calls[0].args[1]).toBe('mute 10')
     })
   })
 })
